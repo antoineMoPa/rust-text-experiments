1f7fedf75b7a4c82d55dd17344d63a7be572f4cd
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2025-01-21 10:52:34 -0500

    model is not working, but at least its trying

--- src/candle_model.rs
+++ src/candle_model.rs
@@ -1,46 +1,28 @@
 use std::io::Error;
 
+use rand::Rng;
+
 use candle_core::{Device, Tensor, DType};
 use candle_nn as nn;
 use nn::{VarMap, Optimizer, VarBuilder, ParamsAdamW};
 
 struct Mlp {
     fc1: nn::Linear,
-    fc2: nn::Linear,
+    act: candle_nn::Activation,
 }
 
 impl Mlp {
-    fn new(device: Device) -> Self {
-        let weight = Tensor::randn(0.5, 0.5, (8, 8), &device.clone()).unwrap();
-        let fc1 = nn::Linear::new(weight, None);
+    fn new(vb: VarBuilder) -> Result<Self, candle_core::Error> {
+        let fc1 = nn::linear(8, 4,vb.pp("fc1"))?;
+        let act = candle_nn::activation::Activation::Relu;
 
-        let weight = Tensor::randn(0.5, 0.5, (4, 8), &device.clone()).unwrap();
-        let fc2 = nn::Linear::new(weight, None);
-
-        Self { fc1, fc2 }
+        Ok(Self { fc1, act })
     }
 
     fn forward(&self, input: &Tensor) -> Result<Tensor, candle_core::Error> {
         input
             .apply(&self.fc1)?
-            .apply(&self.fc2)
-    }
-}
-
-// Define a simple feedforward network
-struct XORNet {
-    mlp: Mlp,
-}
-
-impl XORNet {
-    fn new(device: &Device) -> Result<Self, Error> {
-        let mlp = Mlp::new(device.clone());
-
-        Ok(Self { mlp })
-    }
-
-    fn forward(&self, input: &Tensor) -> Result<Tensor, candle_core::Error> {
-        self.mlp.forward(input)
+            .apply(&self.act)
     }
 }
 
@@ -49,36 +31,64 @@ fn build_and_train_model() -> Result<(), candle_core::Error> {
     let device = Device::Cpu;
 
     // Define training data for XOR
-    // let inputs = Tensor::new(&[[0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0]], &device)?;
-    // let targets = Tensor::new(&[[0.0, 1.0, 1.0, 0.0]], &device)?;
-
-    // Define more training data for XOR
     let mut inputs = Vec::new();
     let mut targets = Vec::new();
 
-    for _ in 0..100 {
-        inputs.push(Tensor::new(&[[0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0]], &device)?);
-        targets.push(Tensor::new(&[[0.0, 1.0, 1.0, 0.0]], &device)?);
+    for _ in 0..4000 {
+        // Generate random sample
+        let mut sample = Vec::new();
+        for _ in 0..8 {
+            sample.push(rand::thread_rng().gen_range(0..2) as f64);
+        }
+        let mut sample_result = Vec::new();
+        // XOR pairs of values
+        for i in 0..4 {
+            let a = sample[(i * 2)] as i32;
+            let b = sample[(i * 2) + 1] as i32;
+            let result = a ^ b;
+
+            sample_result.push(result as f64);
+        }
+
+        println!("Sample: {:?}, Result: {:?}", sample, sample_result);
+
+        inputs.push(Tensor::new(&[
+            sample[0],
+            sample[1],
+            sample[2],
+            sample[3],
+            sample[4],
+            sample[5],
+            sample[6],
+            sample[7],
+        ], &device)?);
+        targets.push(Tensor::new(&[
+            sample_result[0],
+            sample_result[1],
+            sample_result[2],
+            sample_result[3],
+        ], &device)?);
     }
 
     let inputs = Tensor::stack(&inputs, 0)?;
     let targets = Tensor::stack(&targets, 0)?;
 
+    // Create Varbuilder
+    let varmap = VarMap::new();
+    let vb = VarBuilder::from_varmap(&varmap, DType::F64, &Device::Cpu);
+
     // Create the XORNet model
-    let mut model = XORNet::new(&device)?;
+    let model = Mlp::new(vb)?;
 
     // Optimizer settings
-    let varmap = VarMap::new();
-    let vb = VarBuilder::from_varmap(&varmap, DType::F32, &Device::Cpu);
     let params = ParamsAdamW {
-        lr: 0.0001,
+        lr: 0.02,
         ..Default::default()
     };
-    let varmap = VarMap::new();
     let mut optimizer = candle_nn::AdamW::new(varmap.all_vars(), params)?;
 
     // Training loop
-    for epoch in 0..1000 {
+    for epoch in 0..100 {
         // Forward pass
         let predictions = model.forward(&inputs)?;
 
