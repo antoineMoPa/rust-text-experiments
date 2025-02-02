f38b565ad754777b3e26592a8559b61435bdafac
Antoine M-P <antoine@lumen5.com>
2025-02-01 22:25:56 -0500

    Attention wip

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use std::fs;
 
 use crate::{
     token_utils::{tokenize, tokens_to_dict},
-    simple_predictor::{create_and_train_predictor_model, get_device}
+    attention_predictor::{create_and_train_predictor_model, get_device}
 };
 
 mod token_utils;
 mod simple_predictor;
 mod lstm_predictor;
+mod attention_predictor;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Read args
