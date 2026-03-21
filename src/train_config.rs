e5638749e668b3014cb0c86290056bfdc038448b
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2026-03-21 07:43:20 -0400

    tweak lr

--- src/train_config.rs
+++ src/train_config.rs
@@ -27,7 +27,7 @@ impl Default for TrainConfig {
             ffn_hidden: 512,
             num_blocks: 3,
             file_path: "smoll-generated-corpus/level_5/corpus.corpus".to_string(),
-            lr: 0.008,
+            lr: 3.54e-5, // use lr-range-test
             warmup_batches: 600,
             epochs: 6,
             token_batch_size: 32768,
